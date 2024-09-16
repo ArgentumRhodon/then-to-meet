62eb030bd95479bc266c6eb5c0b2b9a5ce15b999
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-09-16 10:11:01 -0400

    improved onboarding

--- src/lib/components/NoMeetingLoaded.svelte
+++ src/lib/components/NoMeetingLoaded.svelte
@@ -1,4 +1,13 @@
+<script>
+	import { linkMemory } from '$lib';
+</script>
+
 <div class="card p-4">
 	<h2 class="text-lg text-warning-500">No Meeting Loaded...</h2>
-	<p>To get started, enter the link or ID of a When2meet event in the When2meet Link field.</p>
+	<p>
+		To get started, enter the link or ID of a When2meet event in the When2meet Link field.
+		{#if $linkMemory.length > 0}
+			<span>Or select a previously loaded meeting.</span>
+		{/if}
+	</p>
 </div>

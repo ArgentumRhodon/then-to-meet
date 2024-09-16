86f2938f003092d733895870c1c6a32c31c1c7a3
ArgentumRhodon <lac2559@g.rit.edu>
2024-09-16 00:24:42 -0400

    moved mem link selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,16 @@
 	import TimeTable from '$lib/components/TimeTable.svelte';
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
 
-	let w2mLink = '';
+	let inputLink = '';
+	let selectLink = '-1'; // Value for "Load Previous"
 	let meetingPromise: Promise<void>;
 	const targetScriptNum = 9;
 
-	const loadMeeting = async (meetingLink: string): Promise<void> => {
+	const loadMeeting = async (link: string): Promise<void> => {
 		const meetingIDRegex: RegExp = /\?(\d+-\w+)/g;
 		let match;
 
-		if ((match = meetingIDRegex.exec(meetingLink)) !== null) {
+		if ((match = meetingIDRegex.exec(link)) !== null) {
 			try {
 				const meetingID = match[0];
 				const htmlRes = await fetch(`/api/w2m/${meetingID}`);
@@ -36,7 +37,7 @@
 				// Other info about the meeting
 				$info = {
 					title: dom.title.split(' - ')[0],
-					link: w2mLink
+					link: inputLink
 				};
 				// If link hasn't been recorded, record it
 				linkMemory.update((arr) => {
@@ -57,35 +58,51 @@
 		}
 	};
 
-	const onLinkSubmit = (e: SubmitEvent): void => {
-		e.preventDefault();
-		meetingPromise = loadMeeting(w2mLink);
+	const loadLink = (link: string): void => {
+		meetingPromise = loadMeeting(link);
+	};
+
+	const loadSelect = () => {
+		if (selectLink != '-1') {
+			loadLink(selectLink);
+		}
 	};
 </script>
 
 <div class="h-full flex flex-col lg:flex-row">
 	<div class="bg-surface-900 lg:w-96 min-w-96 h-min lg:h-full p-4 shadow-2xl">
-		<form
-			class="input-group sm:grid-cols-[auto_1fr_auto] lg:grid-cols-none"
-			on:submit={onLinkSubmit}
-		>
+		<div class="input-group sm:grid-cols-[auto_1fr_auto] lg:grid-cols-none">
 			<label for="link" class="input-group-shim p-4">When2meet Link</label>
 			<input
 				type="text"
 				name="link"
 				class="input p-4"
 				placeholder="https://www.when2meet.com/..."
-				bind:value={w2mLink}
+				bind:value={inputLink}
 			/>
-			{#if $linkMemory.length}
-				<select name="memorizedLink" id="" class="select !bg-surface-800" bind:value={w2mLink}>
-					{#each $linkMemory as memLink}
-						<option value={memLink.link}>{memLink.title}</option>
+			<button class="btn variant-filled-primary p-4" on:click={() => loadLink(inputLink)}>
+				Load Meeting
+			</button>
+		</div>
+
+		{#if $linkMemory.length > 0}
+			<hr class="my-4" />
+			<div class="input-group sm:grid-cols-[auto_1fr] lg:grid-cols-none">
+				<!-- <label for="linkMemory" class="input-group-shim p-4">Load Previous</label> -->
+				<select
+					class="select p-4"
+					name="linkMemory"
+					id=""
+					bind:value={selectLink}
+					on:change={loadSelect}
+				>
+					<option value="-1" selected>Load Previous</option>
+					{#each $linkMemory as { link, title }}
+						<option value={link}>{title}</option>
 					{/each}
 				</select>
-			{/if}
-			<button class="btn variant-filled-primary p-4">Load Meeting</button>
-		</form>
+			</div>
+		{/if}
 
 		<hr class="my-4" />
 

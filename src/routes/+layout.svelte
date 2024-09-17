876862c7430c0b57264dc9fb0c1c49f17cb2af00
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-09-17 13:12:10 -0400

    open beta message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 </script>
 
 <AppShell>
+	<svelte:fragment slot="header">
+		<AppBar background="bg-warning-500">
+			<p class="text-black text-lg">
+				This website is in open beta and might have issues. Please submit any feedback at the
+				<a class="underline" href="https://forms.gle/XV7RAXsSJfgE9EBJ8" target="_blank"
+					>open beta feedback form</a
+				>.
+			</p>
+		</AppBar>
+	</svelte:fragment>
 	<slot />
 </AppShell>

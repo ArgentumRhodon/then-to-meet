2da226e212abf22ae054dcde7fdcda507ac9b3ed
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-10-05 20:10:27 -0400

    fixed meeting time selection

--- src/lib/components/TimePrinter.svelte
+++ src/lib/components/TimePrinter.svelte
@@ -19,7 +19,11 @@
 		}
 	};
 
+	// Returns true if everyone is selected people is within a given slot's availability
 	const everyoneAvailable = (slot: SlotData) => {
+		if (!slot) return false;
+		if (!$selectedPeople.size) return false;
+
 		return [...$selectedPeople].every((person) => slot.available.includes(person.id));
 	};
 
@@ -36,25 +40,24 @@
 
 				// Move along continuous slots
 				let lookForward = i;
-				while (everyoneAvailable(daySlots[lookForward]) && lookForward < daySlots.length - 1) {
+				while (everyoneAvailable(daySlots[lookForward + 1])) {
 					lookForward++;
 				}
 
 				// If difference is less than min, move to next bad slot
-				const difference = lookForward - i - 1;
+				const difference = lookForward - i;
 				if (difference < minSlotDiff) {
-					i = lookForward - 1;
+					i = lookForward;
 					continue;
 				}
 
 				// Initialize new array of meetings for a given day
 				if (!meetings[day]) meetings[day] = new Array<MeetingData>();
 
-				// If length of continouous slots is less than max length, set max length to
-				// length of cont slots for this round
-				maxSlotDiff = Math.min(maxSlotDiff, difference);
+				// Determine max difference between found and specified
+				let maxDifference = Math.min(difference, maxSlotDiff);
 				// Add all meetings with start of i slot with length between max and min
-				for (let j = i + minSlotDiff; j <= i + maxSlotDiff; j++) {
+				for (let j = i + minSlotDiff; j <= i + maxDifference; j++) {
 					meetings[day].push({
 						start: daySlots[i].time,
 						end: daySlots[j].time,

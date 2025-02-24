313c1748e3b0c40b11735ae8a2401a7da022ba95
ArgentumRhodon <lac2559@g.rit.edu>
2025-02-23 22:03:10 -0500

    changed link form submit behavior

--- src/lib/components/LinkForm.svelte
+++ src/lib/components/LinkForm.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	export let loadLink: (link: string) => void;
 
 	let inputLink: string = '';
+
+	const onSubmit = (e: Event) => {
+		e.preventDefault();
+
+		const meetingIDRegex: RegExp = /(?<=\?)\d+-\w+/g;
+
+		let match;
+
+		if ((match = meetingIDRegex.exec(inputLink)) !== null) {
+			try {
+				console.log(match[0]);
+				loadLink('?' + match[0]);
+				goto(`/?${encodeURIComponent(match[0])}`);
+			} catch (e) {
+				throw new Error(
+					'Failed to load link/ID. Ensure it is correct and check your network connection.'
+				);
+			}
+		} else {
+			throw new Error('Bad link/ID.');
+		}
+	};
 </script>
 
-<form
-	class="input-group sm:grid-cols-[auto_1fr_auto] lg:grid-cols-none"
-	on:submit={() => loadLink(inputLink)}
->
+<form class="input-group sm:grid-cols-[auto_1fr_auto] lg:grid-cols-none" on:submit={onSubmit}>
 	<label for="link" class="input-group-shim p-4">When2meet Link</label>
 	<input
 		type="text"

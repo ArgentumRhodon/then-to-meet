5f9235773e71f0b344d3a8be8057a00b3c58a0a1
ArgentumRhodon <lac2559@g.rit.edu>
2025-09-15 02:38:30 -0400

    add page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,15 @@
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { info } from '$lib';
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+    $:title = $info?.title ? `${$info.title} - ThenToMeet` : 'ThenToMeet';
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
 
 <AppShell>
 	<slot />
